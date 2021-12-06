121c17c3955d8f6c0a8eb89ae27c7ce64e558f41
maksimdotcom <ya.maksim0511@gmail.com>
2021-12-06 20:32:35 +0300

    Add /visit

--- app.rb
+++ app.rb
@@ -12,7 +12,25 @@ end
 class Barber < ActiveRecord::Base 
 end
 
+before do 
+	@barbers = Barber.all
+end
+
 get '/' do
-	@barbers = Barber.order "created_at desc"
 	erb :index
+end
+
+get '/visit' do
+
+	erb :visit
+end
+ 
+post '/visit' do 
+	@user_name = params[:user_name]
+	@phone     = params[:phone]
+	@date_time = params[:date_time]
+	@barber    = params[:barber]
+	@color     = params[:color]
+
+	
 end
